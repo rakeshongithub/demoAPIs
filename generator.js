b6b05f0c0be34a1687dfe728eefd149879f72f6d
Rakesh Kumar <rakeshpersonal@gmail.com>
2017-09-03 11:10:46 +0530

    update demo api for coffeshops and reports

--- generator.js
+++ generator.js
@@ -11,6 +11,23 @@ module.exports = function () {
         "phone": faker.phone.phoneNumberFormat(),
         "info": faker.lorem.paragraph()
       }
+    }),
+    reports: _.times(10, function(n) {
+      return {
+        "id": faker.random.uuid().replace(/-/g, ''),
+        "type": faker.finance.currencyCode().substr(0, 3),
+        "reporter": "Exch" + faker.random.number(10),
+        "ID": "1234567",
+        "status": "Active",
+        "memberAliases": [ "FRMA", "FRMA1", "FRMA:U01", "FRMA:U02" ],
+        "userReports": _.times(10, function(n) {
+          return {
+            "date": faker.date.between((2010 + n), (2011 + n)),
+        		"companyName": "The Absolute Best Company Description",
+            "reports": faker.lorem.paragraph()
+          }
+        })
+      }
     })
   }
 }
